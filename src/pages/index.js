3935a550fcff8018c49ff43ca4261a920e920a3d
Luke Gaskell <lukejgaskell@gmail.com>
2021-05-17 01:45:13 -0400

    added react refernce

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,12 @@ import * as React from "react"
 import { Canvas, useFrame } from "@react-three/fiber"
 
 const IndexPage = () => {
-  const canvasRef = React.useRef()
-
   function Box(props) {
     // This reference will give us direct access to the THREE.Mesh object
-    const mesh = useRef()
+    const mesh = React.useRef()
     // Set up state for the hovered and active state
-    const [hovered, setHover] = useState(false)
-    const [active, setActive] = useState(false)
+    const [hovered, setHover] = React.useState(false)
+    const [active, setActive] = React.useState(false)
     // Subscribe this component to the render-loop, rotate the mesh every frame
     useFrame((state, delta) => (mesh.current.rotation.x += 0.01))
     // Return the view, these are regular Threejs elements expressed in JSX
